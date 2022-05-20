012317083900a91ee08badbb5acea431b1823614
Olga Tsybikova <tsy2701@gmail.com>
2022-05-20 12:27:02 +0200

    Tested controller function

--- src/server/controllers/kindsControllers.js
+++ src/server/controllers/kindsControllers.js
@@ -2,10 +2,16 @@ require("dotenv").config();
 const debug = require("debug")("kinds:server:controllers:kindsControllers");
 const Kind = require("../../models/Kind");
 
-const getKindsList = async (req, res) => {
-  const kinds = await Kind.find();
-  res.status(200).json(kinds);
-  debug("Kinds collection request received");
+const getKindsList = async (req, res, next) => {
+  try {
+    const kinds = await Kind.find();
+    res.status(200).json(kinds);
+    debug("Kinds collection request received");
+  } catch (error) {
+    error.StatusCode = 404;
+    error.customMessage = "Not found";
+    next(error);
+  }
 };
 
 module.exports = { getKindsList };
---
+++ src/server/controllers/kindsControllers.test.js
@@ -0,0 +1,28 @@
+const Kind = require("../../models/Kind");
+const { getKindsList } = require("./kindsControllers");
+
+describe("Given a getkindsList fucntion", () => {
+  describe("When it receives a request", () => {
+    test("Then it should response with a method status 200 and a mockKind", async () => {
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const kindsMock = [
+        {
+          id: 22,
+          kind: "Buena cookie",
+        },
+      ];
+
+      Kind.find = jest.fn().mockResolvedValue(kindsMock);
+
+      const expectedStatusCode = 200;
+
+      await getKindsList(null, res);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
+      expect(res.json).toHaveBeenCalledWith(kindsMock);
+    });
+  });
+});
